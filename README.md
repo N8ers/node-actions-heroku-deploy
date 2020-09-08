0b6833bc843c87c2ba6964d996173cb7c8e66b6e
nathan sheryak <nathanraysheryak@gmail.com>
2020-09-08 18:19:18 -0400

    get express boiler plate up

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # node-actions-heroku-deploy
+an example of how to use github actions to deploy node server to heroku
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Nathan!')
+})
+
+const niceThings = ['Wow!', 'Great work', 'You did it!', 'Pizza party?!', 'High Five', 'Gold star!', 'Way to be!!'];
+const getNiceThing = () => {
+  return niceThings[Math.floor(Math.random() * Math.floor(niceThings.length))];
+};
+
+app.get('/niceThing', (req, res) => {
+  res.send(getNiceThing())
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
